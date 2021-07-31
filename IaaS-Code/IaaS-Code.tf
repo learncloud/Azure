94de5bd3721bf6d03f52b0de76b7938934a9fa11
JaeHeon <56222864+learncloud@users.noreply.github.com>
2021-07-31 12:27:45 +0900

    Update IaaS-Code.tf

--- IaaS-Code/IaaS-Code.tf
+++ IaaS-Code/IaaS-Code.tf
@@ -46,7 +46,7 @@ resource "azurerm_network_security_group" "nsgrule" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
-    
+    //멀티 nsg
     tags = {
         environment = "Terraform Demo"
     }
